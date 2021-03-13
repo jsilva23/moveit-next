7f000d7f13f4e0eb278994765489269e0e710214
José da Silva <josesilva23p@gmai..com>
2021-03-13 19:01:10 +0100

    Countdown component

---
+++ src/pages/index.tsx
@@ -0,0 +1,27 @@
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import styles from "../styles/pages/home.module.css";
+
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      <ExperienceBar />
+
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <CountDown />
+        </div>
+        <div></div>
+      </section>
+    </div>
+  );
+}
